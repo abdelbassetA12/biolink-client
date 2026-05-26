c12882f87346cb598235dee4a11b5572203fe699
abdelbassetA12 <abdelbassetelhajiri2020@gmail.com>
2026-05-26 19:15:31 +0100

    Update client

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,8 @@
-const API_BASE = "https://biolink-server-he40.onrender.com";
+//const API_BASE = "https://biolink-server-he40.onrender.com";
 //const API_BASE = "http://localhost:5001";
+//export default API_BASE;
+
+
+const API_BASE = import.meta.env.VITE_API_URL;
+
 export default API_BASE;
